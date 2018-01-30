406c47416c091016859d7e99289eb0c79b3fd9ad
Eloisss <32861885+Eloisss@users.noreply.github.com>
2018-01-31 02:23:30 +0300

    Rename ConsoleApplication4.cpp to helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+﻿// ConsoleApplication4.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	string name;
+	cout << "What's your name?" << endl;
+	cin >> name;
+	cout << "Hey there ," << name << "!\n" << endl;
+	сin.get();
+}
